8e6ae3930c638cf58be5b190f2692263f57c4fdd
vishwajeet kumar <vishwajeet kumar@DESKTOP-U9149QC>
2019-04-13 14:21:44 +0530

    printing hello world and adding two numbers

--- src/main/java/main/SampleProject.java
+++ src/main/java/main/SampleProject.java
@@ -1,18 +1,24 @@
 package main;
-
+//import java.util.*;
 public class SampleProject {
 	//print  Hello world! in the console
 	public static String printHello() {
 		//print Hello world! in the console!
-		String c = "Hello rvabddld!";
+		String c = "Hello World!";
 		return c;
 	}
+	
 	public static int add(int a,int b) {
 		int c = a+b;
 		return c;
 	}
 	public static void main(String args[]) {
 		System.out.println(printHello());
-		System.out.println(add(23,45));
+		//Scanner sc =new Scanner(System.in);
+		//int aa =sc.nextInt();
+		//int bb =sc.nextInt();
+		System.out.println(add(46,3));
+		//sc.close();
 	}
+	
 }
